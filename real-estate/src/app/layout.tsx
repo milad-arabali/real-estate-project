1920d8d3d09ad421db72f0289bdf88e333c7e7a7
milad <miladarabali1@gmail.com>
2025-11-28 16:38:40 +0330

    rs-4 # install-shadcn

--- real-estate/src/app/layout.tsx
+++ real-estate/src/app/layout.tsx
@@ -1,10 +1,31 @@
 import "./globals.css";
 import React from "react";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: "Home",
+    description: "This is home page",
+    keywords: ["nextjs", "typescript", "seo"],
+    robots: {
+        index: true,
+        follow: true,
+    },
+    viewport: "width=device-width, initial-scale=1",
+    authors: [{name: "Milad", url: "https://example.com"}],
+    openGraph: {
+        title: "Home OG Title",
+        description: "OG Description",
+        url: "https://example.com",
+        siteName: "MySite",
+        locale: "fa_IR",
+        type: "website",
+    },
+};
 
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
-        <html lang="fa" dir="rtl" >
+        <html lang="fa" >
         <body>{children}</body>
         </html>
     );
