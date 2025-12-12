23b67966690ac5c67413d34a33bcd0d3639b24aa
milad <miladarabali1@gmail.com>
2025-12-12 16:46:55 +0330

    rs-20 # refactor

--- real-estate/src/app/(account)/layout.tsx
+++ real-estate/src/app/(account)/layout.tsx
@@ -14,21 +14,41 @@ export default async function DashboardLayout({children}: DashboardLayoutProps) 
     const session = await getServerSession(authOptions);
     if (!session) redirect("/");
 
-
     return (
-        <section dir="rtl" className="min-h-screen bg-gray-50 flex justify-center items-center py-10">
-            <div className="flex max-w-7xl w-full mx-auto gap-6">
-                <aside className="w-80 bg-white rounded-3xl shadow-lg p-8 sticky top-6 min-h-[600px] flex flex-col">
+        <section
+            dir="rtl"
+            className="min-h-screen bg-gray-50 flex justify-center items-center py-10"
+        >
+            <div
+                className="
+                flex max-w-7xl w-full mx-auto gap-6
+                flex-col lg:flex-row
+                px-4 sm:px-6
+            "
+            >
 
+                <aside
+                    className="
+                    w-full lg:w-80
+                    bg-white rounded-3xl shadow-lg p-8
+                    sticky top-6
+                    z-10
+                    min-h-[220px] lg:h-[400px]
+                    flex flex-col
+                "
+                >
                     <header className="flex items-center gap-4 !mb-6 !pb-4 !p-2 border-b border-gray-100">
                         <div
-                            className="w-16 h-16 bg-gradient-to-br from-red-400 to-red-600 rounded-full flex
-                            items-center justify-center text-white text-2xl font-bold">
+                            className="w-22 h-16 bg-gradient-to-br from-red-400 to-red-600
+                            rounded-full flex items-center justify-center
+                            text-white text-2xl font-bold mx-auto lg:mx-0"
+                        >
                             س
                         </div>
-                        <div>
+
+                        <div className="text-center lg:text-right w-full">
                             <h3 className="text-xl font-bold text-gray-900">{session.user?.name}</h3>
-                            <p className="text-gray-500 text-[16px]">{session.user?.email}</p>
+                            <p className="text-gray-500 text-[16px] break-all">{session.user?.email}</p>
                         </div>
                     </header>
 
@@ -39,11 +59,11 @@ export default async function DashboardLayout({children}: DashboardLayoutProps) 
                     <nav className="!px-2 !mt-auto !mb-3">
                         <LogoutButton/>
                     </nav>
-
                 </aside>
 
-                <main className="flex-1 p-6 mt-2">{children}</main>
-
+                <main className="flex-1 p-6 mt-2">
+                    {children}
+                </main>
             </div>
         </section>
     );
--- real-estate/src/components/link/NavLinks.tsx
+++ real-estate/src/components/link/NavLinks.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import {usePathname} from "next/navigation";
 import {Heart, Home, ShoppingBag, User} from "lucide-react";
+import {NativeSelect, NativeSelectOption,} from "@/components/ui/native-select";
 
 const links = [
     {href: "/my-account/dashboard", label: "داشبورد", icon: <Home size={20}/>},
@@ -15,19 +16,46 @@ export default function NavLinks() {
     const pathname = usePathname();
 
     return (
-        <nav className="!space-y-3 !px-2">
-            {links.map((link) => (
-                <Link
-                    key={link.href}
-                    href={link.href}
-                    className={`flex items-center gap-4 w-full !m-1 !p-2 rounded-2xl hover:bg-gray-50 ${
-                        pathname === link.href ? "bg-blue-50 text-red-600" : "text-gray-700"
-                    }`}
-                >
-                    {link.icon}
-                    <span className="font-medium">{link.label}</span>
-                </Link>
-            ))}
-        </nav>
+        <div className="w-full lg:w-auto">
+            <nav className="hidden lg:flex flex-col !space-y-3 !px-2">
+                {links.map((link) => (
+                    <Link
+                        key={link.href}
+                        href={link.href}
+                        className={`flex items-center gap-4 w-full !m-1 !p-2 rounded-2xl hover:bg-gray-50 transition-colors ${
+                            pathname === link.href ? "bg-blue-50 text-red-600" : "text-gray-700"
+                        }`}
+                    >
+                        {link.icon}
+                        <span className="font-medium">{link.label}</span>
+                    </Link>
+                ))}
+            </nav>
+
+            <div className="lg:hidden !w-full !mb-4 !px-2">
+                <div className="lg:hidden w-full mb-4 px-2">
+                    <NativeSelect
+                        className="w-full"
+                        value={pathname}
+                        onChange={(e) => {
+                            const value = e.target.value;
+                            if (value) window.location.href = value;
+                        }}
+                    >
+                        <NativeSelectOption value="">منو</NativeSelectOption>
+                        {links.map((link) => (
+                            <NativeSelectOption
+                                className="!px-4 !py-2 "
+                                key={link.href} value={link.href}>
+
+                                {link.label}
+                            </NativeSelectOption>
+                        ))}
+                    </NativeSelect>
+                </div>
+
+
+            </div>
+        </div>
     );
 }
